1d0d4bc24fe8dfb92f459687e2f9ff0129f9f0e7
unknown <michaeljamescarnaje1@gmail.com>
2021-08-15 13:58:39 +0800

    :bento: added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Syntac</title>
+        <link rel="icon" href="favicon/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <title>Syntac Tech</title>
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,6 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
